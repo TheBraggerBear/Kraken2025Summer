bc0f17d811fefb7d4cdb321dd46333b858ae64fd
Aiden Burgstahler <aidenburgstahler@outlook.com>
2025-07-02 20:53:53 -0500

    Update logic for the singleSolenoids

--- Kraken2025/src/main/java/frc/robot/RobotContainer.java
+++ Kraken2025/src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,9 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.*;
+
+import com.google.flatbuffers.ShortVector;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -57,10 +60,13 @@ public class RobotContainer {
         .whileTrue(shooter.fullSpeed()); // Example speed, adjust as needed;
     m_driverController.leftTrigger()
         .whileTrue(shooter.halfSpeed());
-    m_driverController.a()
-        .whileTrue(intakeArm.setState(IntakeArm.State.RAISED));
-    m_driverController.b()
-        .whileTrue(intakeArm.setState(IntakeArm.State.LOWERED));
+    // m_driverController.a()
+    //     .toggleOnTrue(intakeArm.setState(IntakeArm.State.RAISED).withTimeout(0.5).andThen(intakeArm.setState(IntakeArm.State.OFF)));
+    // m_driverController.b()
+    //     .toggleOnTrue(intakeArm.setState(IntakeArm.State.LOWERED).withTimeout(0.5).andThen(intakeArm.setState(IntakeArm.State.OFF)));
+    m_driverController.x()
+        .toggleOnTrue(intakeArm.changeState());
+    // Add more trigger bindings as needed
   }
 
   /**
